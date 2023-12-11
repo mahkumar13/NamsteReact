3f05e28638f60c41d6042ea233cab75b67997005
Mahendra Kumar <mahendra.kumar@businessnext.com>
2023-12-11 22:59:11 +0530

    added top rated button functionality

--- swiggerrr/src/Components/Body.jsx
+++ swiggerrr/src/Components/Body.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react"
 import resDataList from "../Utils/MockData"
 import ResturantCard from "./ResturantCard"
 
 const Body= ()=>{
+  console.warn(resDataList)
+   const [TopRatedResturant,setTopRatedResturant] =useState(resDataList)
     return(
     <div className="body">
      <div className="search">Search</div>
+     <button className="btn" 
+      onClick={()=>{
+        const filterList= resDataList.filter(
+          (res)=>res.info.avgRating>4
+        );
+        setTopRatedResturant(filterList)
+      }
+      }
+     >TOP RATED Resturant</button>
      <div className="res-container"> 
       
       
-      {resDataList.map(res=>
+      {TopRatedResturant.map(res=>
         <ResturantCard  key={res.info.id} resList={res} > </ResturantCard>)
       }
       
