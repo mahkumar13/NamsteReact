e1be2158aa3885cc7888f601d4bee649b420d52b
Mahendra Kumar <mahendra.kumar@businessnext.com>
2023-12-12 19:02:24 +0530

    episode 6

--- swiggerrr/src/Components/Body.jsx
+++ swiggerrr/src/Components/Body.jsx
@@ -1,31 +1,64 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import resDataList from "../Utils/MockData"
 import ResturantCard from "./ResturantCard"
+import Shimmer from "./Shimmer";
 
 const Body= ()=>{
-  console.warn(resDataList)
-   const [TopRatedResturant,setTopRatedResturant] =useState(resDataList)
+   const [ListofResturant,setListOfResturant]= useState([]);
+   const [TopRatedResturant,setTopRatedResturant] =useState([])
+   const [serachText,setSerachText]=useState("")
+
+    useEffect(()=>{
+        fetchData();
+    },[])
+    const fetchData=  async()=>{
+      const data = await fetch(" https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6034084&lng=77.3782031&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+       const json=  await data.json();
+       setListOfResturant(json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants)
+
+       console.log(json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants
+        )
+    }
+    if(ListofResturant.length===0){
+      return (
+          <Shimmer></Shimmer>
+      )
+    }
+    
     return(
     <div className="body">
-     <div className="search">Search</div>
+      <div className="filter">
+     <div className="search">Search
+     
+     <input type="text" className="search-box" placeholder="" value={serachText}
+     onChange={(e)=>setSerachText(e.target.value)}
+     ></input>
+     <button  className="serach-btn"
+       onClick={()=>{
+         console.log(serachText)
+       }}
+     > Serach</button>
+     </div>
+
      <button className="btn" 
       onClick={()=>{
-        const filterList= resDataList.filter(
+        const filterList= ListofResturant.filter(
           (res)=>res.info.avgRating>4
         );
         setTopRatedResturant(filterList)
+        setListOfResturant(filterList)
       }
       }
      >TOP RATED Resturant</button>
+     </div>
      <div className="res-container"> 
-      
-      
-      {TopRatedResturant.map(res=>
+      {ListofResturant.map(res=>
         <ResturantCard  key={res.info.id} resList={res} > </ResturantCard>)
       }
       
      </div>
     </div>
+    
     )
   }
  
--- swiggerrr/src/Components/Header.jsx
+++ swiggerrr/src/Components/Header.jsx
@@ -1,5 +1,8 @@
- import {LOGO_URL} from '../Utils/constants'
+ import { useState } from 'react'
+import {LOGO_URL} from '../Utils/constants'
 const Header= ()=>{
+
+  const [buttonText,setButtonText]=useState("LogIn")
     return (
       <div className='header'>
       <div className='logo-container'>
@@ -11,6 +14,11 @@ const Header= ()=>{
       <li>About Us</li>
       <li>Contact Us </li>
       <li>Cart</li>
+      <button onClick={()=>{
+        buttonText==="LogIn"?setButtonText("LogOut"):setButtonText("LogIn")
+       
+      }}
+      className='login'>{buttonText}</button>
      </ul>
       </div>
       </div>
