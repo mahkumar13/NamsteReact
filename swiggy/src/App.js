adf62e6e129fea02db7194c59f5f905884b0c186
Mahendra Kumar <mahendra.kumar@businessnext.com>
2024-02-10 00:09:43 +0530

    swiggy project

---
+++ swiggy/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="text-3xl font-bold underline text-red-700" > hello</h1>
+    </div>
+  );
+}
+
+export default App;
